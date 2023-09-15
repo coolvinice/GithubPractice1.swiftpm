63613ec9a190e88d69bce8ddbd915582af47f0a6
Calvin Rice <ramen.calvin@yahoo.com>
2023-09-15 11:16:58 -0500

    task list

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var tasks: [String] = ["Mow the Lawn", "Feed the  Dog", "Grocery Shop"]
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List {
+                ForEach(tasks, id: \.self) { task in
+                    Text(task)
+                }
+            }
         }
     }
 }
