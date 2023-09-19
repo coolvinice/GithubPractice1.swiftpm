f4a13d139c7c1ff6d338dd982dbe69e3c6988179
Calvin Rice <ramen.calvin@yahoo.com>
2023-09-19 10:52:01 -0500

    STUFF LOOKS BETTER

--- ContentView.swift
+++ ContentView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ContentView: View {
     @State var tasks: [Task] = [Task(nameOfTask: "Mow the Lawn"), Task(nameOfTask: "Feed the Dog"), Task(nameOfTask: "Grocery Shop")]
     var body: some View {
+        HeaderView(tasks: $tasks, enteredString: "")
         VStack {
             List {
                 ForEach(tasks, id: \.self) { task in
