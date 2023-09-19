26d1f3c8cef6a3ff3137689f1bf5ea05f11d115d
Calvin Rice <ramen.calvin@yahoo.com>
2023-09-19 10:44:41 -0500

    Added HeaderView

---
+++ HeaderView.swift
@@ -0,0 +1,25 @@
+//
+//  HeaderView.swift
+//  GithubPractice1
+//
+//  Created by Calvin Rice on 9/19/23.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    @Binding var tasks: [Task]
+    @State var enteredString: String;
+    var body: some View {
+        HStack {
+            TextField("New Task", text: $enteredString)
+            Button {
+                tasks.append(Task(nameOfTask: enteredString))
+            } label: {
+                Text("Add to List")
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 25))
+            }
+        }
+    }
+}
